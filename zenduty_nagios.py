b3d174b9d0dbce28dcf50a303b45a6ec3cc71523
Amodh Shenoy <32415315+AmodhShenoy@users.noreply.github.com>
2019-06-20 17:25:02 +0530

    Commited new script files

---
+++ zenduty_nagios.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+import sys, os
+import argparse
+import urllib2
+import json
+
+
+def build_arguments_parser(description):
+    parser = argparse.ArgumentParser(description)
+
+    parser.add_argument('-n', '--token', 
+                        dest="token",
+                        required=True,
+                        help="Zenduty unique token assigned to this service")
+    parser.add_argument('-e', '--notification-source',
+                        dest="notification_source",
+                        required=True,
+                        help="nagios notification source (host/service)",
+                        choices=["service", "host"])
+    parser.add_argument('-t', "--notification-type",
+                        dest="notification_type",
+                        required=True,
+                        help='nagios notification-type ("PROBLEM", "RECOVERY", "FLAPPINGSTART", etc ...)')
+    parser.add_argument('-f', '--additional-fields',
+                        action="append",
+                        dest="field",
+                        help="additional fields/details to send")
+    return parser 
+
+
+def get_fields(field_array):
+    if field_array is None:
+        return {}
+    to_ret=dict(f.split("=", 1) for f in field_array)
+    return to_ret
+    
+
+
+def main():
+    try:
+        description = "collect arguments for zenduty"
+        parser = build_arguments_parser(description)
+        args = parser.parse_args()
+        data_to_send = {
+            "token": args.token,
+            "notification_type": args.notification_type,
+            "notification_source": args.notification_source,
+            "fields": get_fields(args.field)
+        }
+        url = "https://www.zenduty.com/api/integration/nagios/{}/".format(args.token)
+        data = json.dumps(data_to_send)
+        req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
+        f = urllib2.urlopen(req)
+        response = f.read()
+        f.close()
+        exit(0)
+    except Exception as e:
+        print(str(e))
+        exit(4)
+
+
+main()
